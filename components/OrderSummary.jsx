04e2f82562b8f4fbb68b709893b94d910c250db9
Adarsh Shaji <adarsh.shj63@gmail.com>
2025-08-12 11:42:14 +0530

    Order creation bug fix

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useAppContext } from "@/context/AppContext";
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const OrderSummary = () => {
 
@@ -37,6 +38,41 @@ const OrderSummary = () => {
   };
 
   const createOrder = async () => {
+
+    try {
+
+      if(!selectedAddress){
+        return toast.error('Please select an address')
+      }
+
+      let cartItemsArray = Object.keys(cartItems).map((key) => ({product:key,quantity:cartItems[key]}))
+
+      cartItemsArray = cartItemsArray.filter(item => item.quantity > 0)
+
+      if(cartItemsArray.length === 0){
+        return toast.error('Cart is Empty')
+      }
+
+      const token = await getToken()
+
+      const { data } = await axios.post('/api/order/create',{
+        address: selectedAddress._id,
+        items: cartItemsArray
+      },{
+        headers: {Authorization:`Bearer ${token}`}
+      })
+      
+      if(data.success){
+        toast.success(data.message)
+        setCartItems({})
+
+        router.push('/order-placed')
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
 
   }
 
